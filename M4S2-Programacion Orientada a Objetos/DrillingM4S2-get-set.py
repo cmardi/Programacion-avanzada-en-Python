838ce35917ccaed8e0d56953910815524ca3f1af
Carlos Mamani Ardiles <cmamani2003@gmail.com>
2024-10-05 01:02:16 -0300

    "Importando tabulate"

--- M4S2-Programacion Orientada a Objetos/DrillingM4S2-get-set.py
+++ M4S2-Programacion Orientada a Objetos/DrillingM4S2-get-set.py
@@ -1,3 +1,5 @@
+from tabulate import tabulate
+
 class Persona:
     def __init__(self, nombre, apellido, sexo, edad, estatura, peso ):
         self._nombre = nombre
@@ -24,26 +26,31 @@ class Persona:
     def get_apellido(self):
         return self._apellido
 
-    def __str__(self):
-        return (f"Nombre: {self._nombre} {self._apellido}\n"
-                f"Sexo: {self._sexo}\n"
-                f"Edad: {self._edad}\n"
-                f"Estatura: {self._estatura}\n"
-                f"Peso: {self._peso}")
+    # def __str__(self):
+    #     return (f"Nombre: {self._nombre} {self._apellido}\n"
+    #             f"Sexo: {self._sexo}\n"
+    #             f"Edad: {self._edad}\n"
+    #             f"Estatura: {self._estatura}\n"
+    #             f"Peso: {self._peso}")
+
+    def to_list(self):
+        return(self._nombre, self._apellido, self._sexo, self._edad, self._estatura, self._peso)
+
 
 persona_1 = Persona("Pedro", "Vivas", "Masculino", "20 años", "1.78 mts", "68 Kg.")
 persona_2 = Persona("Juan", "Camargo", "Masculino", "18 años", "1.8 mts", "75 Kg.\n")
 
-print("\nAntes de las modificaciones:")
-print(persona_1)
-print()
-print(persona_2)
+print("\nAntes de las modificaciones:\n")
+print(tabulate([persona_1.to_list(), persona_2.to_list()], headers=["Nombre", "Apellido", "Sexo", "Edad", "Estatura", "Peso"], tablefmt="grid"))
+# print(persona_1)
+# print()
+# print(persona_2)
 
 persona_1.set_edad("21 años")
 persona_2.set_apellido("Santiago")
 
-print("Después de las modificaciones:")
-print(persona_1)
-print()
-print(persona_2)
+print("\nDespués de las modificaciones:\n")
+print(tabulate([persona_1.to_list(), persona_2.to_list()], headers = ["Nombre", "Apellido", "Sexo", "Edad", "Estatura", "Peso"], tablefmt = "grid"))
+# print()
+# print(persona_2)
     
